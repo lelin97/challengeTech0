d90f0bb7d74e57340895e8898673200e82e39372
Lelo Monteiro <eliezermonteiro97@outlook.com>
2024-06-25 13:59:44 -0300

    feat: add mui date pickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { LocalizationProvider } from "@mui/x-date-pickers";
 import AppRouter from "./routes";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { ptBR } from "date-fns/locale";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 
 export default function App() {
   return (
     <>
       <AppThemeProvider>
-        <AppRouter />
+        <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDateFns}>
+          <AppRouter />
+        </LocalizationProvider>
       </AppThemeProvider>
     </>
   );
