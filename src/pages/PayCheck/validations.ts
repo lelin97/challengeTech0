57e382ec463c7d7d75c4bec3e292d5669f1010dc
Lelo Monteiro <eliezermonteiro97@outlook.com>
2024-06-25 14:00:17 -0300

    feat: add message in validationPayCheck

--- src/pages/PayCheck/validations.ts
+++ src/pages/PayCheck/validations.ts
@@ -1,9 +1,29 @@
-import { format } from "date-fns";
 import { z } from "zod";
 
 export const validationRegisterPayCheck = z.object({
-  descricao: z.string().min(1),
-  dt_venc: z.coerce.date().transform((v) => format(v, "yyyy-MM-dd")),
-  valor_multa: z.number().positive(),
-  juros: z.number().positive(),
+  descricao: z
+    .string({
+      errorMap: () => ({
+        message: "Descrição é um campo obrigatório.",
+      }),
+    })
+    .min(1),
+  dt_venc: z.coerce.date(),
+  valor_multa: z
+    .string({
+      errorMap: () => ({
+        message: "Valor Multa é um campo obrigatório.",
+      }),
+    })
+    .min(1),
+  juros: z
+    .string({
+      errorMap: () => ({
+        message: "Juros é um campo obrigatório.",
+      }),
+    })
+    .min(1),
 });
+
+export type DefaultPayCheckInput = z.input<typeof validationRegisterPayCheck>;
+export type DefaultPayCheckOutput = z.output<typeof validationRegisterPayCheck>;
